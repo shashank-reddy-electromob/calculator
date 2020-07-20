88a8f5be6c39e93cae470991e289b44f12599b4f
manasakandimalla <manasareddykandimalla@gmail.com>
2020-07-20 11:54:10 +0530

    calculator layout

--- app/src/main/java/org/electromob/calculator/MainActivity.java
+++ app/src/main/java/org/electromob/calculator/MainActivity.java
@@ -2,13 +2,29 @@ package org.electromob.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login = findViewById(R.id.button);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),loginactivity.class));
+                Toast.makeText(getApplicationContext(),"welcome",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
---
+++ app/src/main/java/org/electromob/calculator/otp.java
@@ -0,0 +1,14 @@
+package org.electromob.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class otp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_otp);
+    }
+}
