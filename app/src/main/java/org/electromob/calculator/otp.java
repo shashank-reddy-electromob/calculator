057e40c05c83fc046a030a2f06d660a5ef00533b
manasakandimalla <manasareddykandimalla@gmail.com>
2020-07-21 10:57:15 +0530

    calculator layout

--- app/src/main/java/org/electromob/calculator/otp.java
+++ app/src/main/java/org/electromob/calculator/otp.java
@@ -2,13 +2,26 @@ package org.electromob.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.UnderlineSpan;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class otp extends AppCompatActivity {
+
+    TextView resend;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_otp);
+
+        SpannableString content = new SpannableString("resend");
+        content.setSpan(new UnderlineSpan(),0,content.length(),0);
+        resend.setText(content);
     }
 }
